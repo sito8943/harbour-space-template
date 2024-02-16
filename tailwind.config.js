6193db82c576bccfc27741c15b3d4d009c514ae4
Sito8943 <sito8943@gmail.com>
2024-02-16 13:12:56 -0500

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#fbfbfb",
+      primary: "#685dc5",
+      secondary: "#4fa16c",
+    },
+  },
+  plugins: [],
+};
