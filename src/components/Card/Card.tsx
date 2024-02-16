8966a5bea85fa5adedfea2cdc117c6f0ba183a80
Sito8943 <sito8943@gmail.com>
2024-02-16 13:54:35 -0500

    created Card component with styles

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,21 @@
+// styles
+import "./styles.css";
+
+type CardPropType = {
+  className?: string;
+  header?: string;
+  children: React.ReactNode;
+};
+
+function Card(props: CardPropType) {
+  const { className = "", header, children } = props;
+
+  return (
+    <div className={`bordered-box ${className}`}>
+      {header && <h3 className="card-header">{header}</h3>}
+      {children}
+    </div>
+  );
+}
+
+export default Card;
