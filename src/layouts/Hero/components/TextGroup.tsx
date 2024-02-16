722109ff85281f93d87411ad4eb46c824557a913
Sito8943 <sito8943@gmail.com>
2024-02-16 16:43:55 -0500

    created TextGroup component only with the plain text and cta

---
+++ src/layouts/Hero/components/TextGroup.tsx
@@ -0,0 +1,22 @@
+function TextGroup() {
+  return (
+    <>
+      <p className="font-semibold">
+        A fully funded work-study program to launch your tech career{" "}
+      </p>
+      <p>
+        Harbour.Space has partnered with SCG to empower driven talent and
+        eliminate the barriers to accessing exceptional education and career
+        opportunities through a Masters Fellowship.{" "}
+      </p>
+      <p className="my-7">
+        <b>Position</b>: Marketing Performance
+      </p>
+      <a>
+        <button className="cta-primary">Apply now</button>
+      </a>
+    </>
+  );
+}
+
+export default TextGroup;
