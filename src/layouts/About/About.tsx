6630fdd6f40f8b3e8006fc25e6acd83c58522aad
Sito8943 <sito8943@gmail.com>
2024-02-16 17:01:01 -0500

    responsive styles for about info section

--- src/layouts/About/About.tsx
+++ src/layouts/About/About.tsx
@@ -9,7 +9,7 @@ function About() {
   return (
     <section
       id="about"
-      className="section flex flex-col items-center justify-center"
+      className="section flex flex-col items-center justify-center ipadPro:p-0"
     >
       <InfoSection />
       <GridSection />
--- src/layouts/About/components/InfoSection.tsx
+++ src/layouts/About/components/InfoSection.tsx
@@ -7,8 +7,8 @@ import Column from "../../../components/Column/Column";
 
 function InfoSection() {
   return (
-    <div className="flex items-center justify-center gap-[120px] mt-20">
-      <div className="relative flex items-center justify-center">
+    <div className="flex items-center justify-center gap-[120px] mt-20 desktop:gap-20 ipadPro:flex-col ipadPro:gap-10 ipadPro:mt-10">
+      <div className="relative flex items-center justify-center ipadPro:-mb-[50%]">
         <img
           src={circleVectors}
           alt="A set of vectors arranged in a circular pattern."
@@ -16,21 +16,23 @@ function InfoSection() {
         />
         <img
           src={about}
-          className="rounded-full w-[380px] h-[380px] object-cover absolute center-horizontal center-vertical"
+          className="rounded-full w-[380px] h-[380px] object-cover absolute center-horizontal center-vertical z-20"
           alt="Image of a young boy holding a backpack in a Harbor Space campus."
         />
       </div>
-      <Column className="w-[50%]">
-        <h2>
-          About the <br /> apprenticeship
-        </h2>
-        <p>
-          Our scholarships are designed to give talented and driven young people
-          from any background access to top-class education, experience and
-          network. We offer a fully-funded master's degree alongside an
-          apprenticeship and a guaranteed job upon graduation.{" "}
-        </p>
-      </Column>
+      <div className="w-[50%] ipadPro:w-full ipadPro:bg-primary ipadPro:px-10 z-10 max-h-[50vh]">
+        <Column className="bg-white ipadPro:px-10 ipadPro:pt-[200px]">
+          <h2 className="ipadPro:mt-[60px]">
+            About the <br /> apprenticeship
+          </h2>
+          <p>
+            Our scholarships are designed to give talented and driven young
+            people from any background access to top-class education, experience
+            and network. We offer a fully-funded master's degree alongside an
+            apprenticeship and a guaranteed job upon graduation.{" "}
+          </p>
+        </Column>
+      </div>
     </div>
   );
 }
