4fe29705bf17b0d5f75d31349e15cc635b5e3c97
Sito8943 <sito8943@gmail.com>
2024-02-16 15:40:53 -0500

    changed all card-headers to h3 without classes

--- src/layouts/Hero/components/GridCard.tsx
+++ src/layouts/Hero/components/GridCard.tsx
@@ -4,22 +4,22 @@ function GridCard() {
   return (
     <Card className="grid grid-cols-2 w-full gap-2">
       <div>
-        <h3 className="card-header">Location</h3>
+        <h3>Location</h3>
         <p className="card-content">Bangkok</p>
       </div>
       <div>
-        <h3 className="card-header">Duration</h3>
+        <h3>Duration</h3>
         <p className="card-content">
           1 Year <br />
           Full-Time
         </p>
       </div>
       <div>
-        <h3 className="card-header">Start date</h3>
+        <h3>Start date</h3>
         <p className="card-content">30 June 2020</p>
       </div>
       <div>
-        <h3 className="card-header">End date</h3>
+        <h3>End date</h3>
         <p className="card-content">3 Aug 2020</p>
       </div>
     </Card>
