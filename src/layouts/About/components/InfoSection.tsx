1d9397a24950c348b7ab4008cc97a535b6d88e76
Sito8943 <sito8943@gmail.com>
2024-02-16 17:54:55 -0500

    fixed ipad and mobile responsive styles for InfoSection

--- src/layouts/About/components/InfoSection.tsx
+++ src/layouts/About/components/InfoSection.tsx
@@ -8,20 +8,20 @@ import Column from "../../../components/Column/Column";
 function InfoSection() {
   return (
     <div className="flex items-center justify-center gap-[120px] mt-20 desktop:gap-20 desktop:mt-0 ipadPro:flex-col ipadPro:gap-10">
-      <div className="relative flex items-center justify-center ipadPro:-mb-[50%]">
+      <div className="relative flex items-center justify-center ipadPro:-mb-[51%]">
         <img
           src={circleVectors}
           alt="A set of vectors arranged in a circular pattern."
-          className="w-[438px] h-[438px] object-cover object-center"
+          className="w-[438px] h-[438px] ipadPro:w-[90%] ipadPro:h-full object-cover object-center"
         />
         <img
           src={about}
-          className="rounded-full w-[380px] h-[380px] object-cover absolute center-horizontal center-vertical z-20"
+          className="rounded-full w-[380px] h-[380px] ipad:w-[80%] ipad:h-[85%] object-cover absolute center-horizontal center-vertical z-20"
           alt="Image of a young boy holding a backpack in a Harbor Space campus."
         />
       </div>
-      <div className="w-[50%] ipadPro:w-full ipadPro:bg-primary ipadPro:px-10 z-10 max-h-[50vh]">
-        <Column className="bg-white ipadPro:px-10 ipadPro:pt-[200px]">
+      <div className="w-[50%] ipadPro:w-full ipadPro:bg-primary z-10 max-h-[50vh] ipadPro:px-10 ipad:px-5">
+        <Column className="gap-15 bg-white ipadPro:px-10 ipad:px-7 ipadPro:-mb-20 ipadPro:pt-40 ipad:pt-40 mobile:pt-20 ipadPro:pb-20 mobile:pb-10 ipadPro:border-[1px] ipadPro:border-[#DADADA] rounded-b-[5px]">
           <h2 className="ipadPro:mt-[60px]">
             About the <br /> apprenticeship
           </h2>
