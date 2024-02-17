12ef2852ae0a0e220be6c5906d0606047898a7e7
Sito8943 <sito8943@gmail.com>
2024-02-17 11:48:00 -0500

    created StickyBar

---
+++ src/components/StickyBar/StickyBar.tsx
@@ -0,0 +1,35 @@
+// styles
+import "./styles.css";
+
+function StickyBar() {
+  return (
+    <div className="sticky-bar">
+      <div>
+        <h4>Zeptolab</h4>
+        <p>Marketing Performance</p>
+      </div>
+      <div>
+        <h4>Location</h4>
+        <p>Bangkok</p>
+      </div>
+      <div>
+        <h4>Duration</h4>
+        <p>1 Year Full-Time</p>
+      </div>
+      <div>
+        <h4>Start date</h4>
+        <p>3 Aug 2020</p>
+      </div>
+      <div>
+        <h4>Application deadline</h4>
+        <p>30 June 2020</p>
+      </div>
+      <div>
+        <h4>Application closes in</h4>
+        <p>6 Day : 22 Hrs : 56 Min</p>
+      </div>
+    </div>
+  );
+}
+
+export default StickyBar;
