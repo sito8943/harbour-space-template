b1c9f4d9105a81400afd403e8f75e5c94db018c6
Sito8943 <sito8943@gmail.com>
2024-02-18 10:13:51 -0500

    added common dto exports to index of Dto

---
+++ src/utils/models/dto/index.ts
@@ -0,0 +1,22 @@
+import { ApplicationProcess } from "./ApplicationProcess";
+import { Company } from "./Company";
+import { Country } from "./Country";
+import { Director } from "./Director";
+import { Faq } from "./Faq";
+import { Location } from "./Location";
+import { PhotoFile } from "./PhotoFile";
+import { Program } from "./Program";
+import { Scholarship } from "./Scholarship";
+import { Testimonial } from "./Testimonial";
+export type {
+  ApplicationProcess,
+  Company,
+  Country,
+  Director,
+  Faq,
+  Location,
+  PhotoFile,
+  Program,
+  Scholarship,
+  Testimonial,
+};
