71f3c2fec1b407a049600bd32ec4db98de19d6e1
Sito8943 <sito8943@gmail.com>
2024-02-17 20:38:09 -0500

    added new image to hero

--- src/sections/Hero/components/InfoSection.tsx
+++ src/sections/Hero/components/InfoSection.tsx
@@ -1,12 +1,24 @@
 // components
 import Column from "../../../components/Column/Column";
 
+// image
+import hand from "../../../assets/images/hand.png";
+
 function InfoSection() {
   return (
     <Column className="ipadPro:px-5">
-      <h2 className="ipadPro:absolute ipadPro:top-0">
-        Interaction Design <br /> Apprenticeship
-      </h2>
+      <div className="ipadPro:absolute ipadPro:top-0 ipadPro:left-0 ipadPro:pl-10 ipad:pl-5 mobile:pl-3">
+        <div className="relative">
+          <h2>
+            Interaction Design <br /> Apprenticeship
+          </h2>
+          <img
+            src={hand}
+            className="absolute -top-[100px] -right-[100px] ipad:w-[200px] ipad:h-[200px] ipad:-top-[110px] ipad:-right-[70px]"
+            alt="Image of a circle composed of texts arranged in a circular shape, with a hand in the center touching a series of rhombus-shaped vectors."
+          />
+        </div>
+      </div>
       <p className="font-semibold">
         A fully funded work-study program to launch your tech career{" "}
       </p>
