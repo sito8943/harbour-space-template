4ce4393c04c77821748ee16ce7ac02dbc4fab2f3
Sito8943 <sito8943@gmail.com>
2024-02-18 12:20:13 -0500

    fixed imports in About Info Section

--- src/sections/About/components/InfoSection.tsx
+++ src/sections/About/components/InfoSection.tsx
@@ -3,7 +3,7 @@ import about from "assets/images/about.jpg";
 import circleVectors from "assets/images/circle-vectors.jpg";
 
 // components
-import Column from "components/Column/Column";
+import { Column } from "components";
 
 function InfoSection() {
   return (
