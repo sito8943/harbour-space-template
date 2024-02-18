e65804bb2c4a10e7864a4a749147e3dc6ae6792a
Sito8943 <sito8943@gmail.com>
2024-02-18 10:12:53 -0500

    created Country dto

---
+++ src/utils/models/dto/Country.ts
@@ -0,0 +1,9 @@
+export type Country = {
+  id: number;
+  scope: string;
+  country_name: string;
+  display_name: string;
+  country_code: string;
+  country_flag: string;
+  country_phone_code: string;
+};
