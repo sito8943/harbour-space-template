39f47e562f4be7e346cab105671376274e6dca39
Sito8943 <sito8943@gmail.com>
2024-02-18 15:56:40 -0500

    using api data in StickyBar

--- src/components/StickyBar/StickyBar.tsx
+++ src/components/StickyBar/StickyBar.tsx
@@ -1,7 +1,15 @@
+// providers
+import { useScholarship } from "providers";
+
+// utils
+import { parseDate } from "utils";
+
 // styles
 import "./styles.css";
 
 function StickyBar() {
+  const { scholarship } = useScholarship();
+
   return (
     <footer className="sticky-bar">
       <div>
@@ -10,19 +18,19 @@ function StickyBar() {
       </div>
       <div>
         <h4>Location</h4>
-        <p>Bangkok</p>
+        <p>{scholarship.location.name}</p>
       </div>
       <div>
         <h4>Duration</h4>
-        <p>1 Year Full-Time</p>
+        <p>{scholarship.duration} Year Full-Time</p>
       </div>
       <div>
         <h4>Start date</h4>
-        <p>3 Aug 2020</p>
+        <p>{parseDate(scholarship.scholarship_start_date)}</p>
       </div>
       <div>
         <h4>Application deadline</h4>
-        <p>30 June 2020</p>
+        <p>{parseDate(scholarship.application_end_date)}</p>
       </div>
       <div>
         <h4>Application closes in</h4>
