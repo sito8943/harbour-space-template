51cd58175ebd2db53da2de7b7e756bb42af124d5
Sito8943 <sito8943@gmail.com>
2024-02-18 12:15:22 -0500

    created index file with all common components exports

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Card from "./Card/Card";
+import Column from "./Column/Column";
+import Navbar from "./Navbar/Navbar";
+import SplashScreen from "./SplashScreen/SplashScreen";
+import StickyBar from "./StickyBar/StickyBar";
+
+export { Card, Column, Navbar, SplashScreen, StickyBar };
