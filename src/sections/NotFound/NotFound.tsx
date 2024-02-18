368a75b79a7d5b6ce34a8b3414ab389966aca7cb
Sito8943 <sito8943@gmail.com>
2024-02-18 15:02:23 -0500

    created NotFound page

--- src/sections/NotFound/NotFound.tsx
+++ src/sections/NotFound/NotFound.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 function NotFound() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-primary">
+      <h1 className="text-4xl text-white">NOT FOUND</h1>
+    </div>
+  );
 }
 
 export default NotFound;
