25f369b1f9d300633055b91d5def488606a1f997
Sito8943 <sito8943@gmail.com>
2024-02-18 08:21:33 -0500

    created index type file for common exports

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import { Testimonial } from "./Testimonial";
+import { HtmlItem } from "./HtmlItem";
+import { HtmlMetaTag, OGMetaTag } from "./MetaTags";
+export type { Testimonial, HtmlItem, HtmlMetaTag, OGMetaTag };
