12d513a23dc28d1923784c4821b3087b756fd68f
Sito8943 <sito8943@gmail.com>
2024-02-18 12:15:55 -0500

    created index file with all exports for sections

---
+++ src/sections/index.ts
@@ -0,0 +1,6 @@
+import About from "./About/About";
+import Hero from "./Hero/Hero";
+import Testimonials from "./Testimonials/Testimonials";
+import Faqs from "./Faqs/Faqs";
+
+export { About, Hero, Testimonials, Faqs };
