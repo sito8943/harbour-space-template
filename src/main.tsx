ba6d127ffab33a333b252b13031d5c2ccc2ef94a
Sito8943 <sito8943@gmail.com>
2024-02-18 13:11:04 -0500

    added routes to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 // utils
@@ -20,7 +21,11 @@ const harbourSpaceClient = new HarbourSpaceApiClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <HarbourSpaceApiClientProvider client={harbourSpaceClient}>
-      <ScholarshipView />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/:slug" element={<ScholarshipView />} />
+        </Routes>
+      </BrowserRouter>
     </HarbourSpaceApiClientProvider>
   </QueryClientProvider>
 );
