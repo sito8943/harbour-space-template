bdf1e8fedd53b3e161a9acc3dfb3cddfddd412b6
Sito8943 <sito8943@gmail.com>
2024-02-18 11:59:29 -0500

    changing imports to relative imports

--- src/ScholarshipView.tsx
+++ src/ScholarshipView.tsx
@@ -1,13 +1,13 @@
 // components
-import Navbar from "./components/Navbar/Navbar";
-import StickyBar from "./components/StickyBar/StickyBar";
+import Navbar from "components/Navbar/Navbar";
+import StickyBar from "components/StickyBar/StickyBar";
 import SplashScreen from "components/SplashScreen/SplashScreen";
 
 // sections
-import Hero from "./sections/Hero/Hero";
-import About from "./sections/About/About";
-import Testimonials from "./sections/Testimonials/Testimonials";
-import Faqs from "./sections/Faqs/Faqs";
+import Hero from "sections/Hero/Hero";
+import About from "sections/About/About";
+import Testimonials from "sections/Testimonials/Testimonials";
+import Faqs from "sections/Faqs/Faqs";
 
 // providers
 import { ScholarshipProvider } from "providers/Scholarship/ScholarshipProvider";
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import ScholarshipView from "./ScholarshipView.js";
-import "./index.css";
+import ScholarshipView from "ScholarshipView";
+import "index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ScholarshipView />
