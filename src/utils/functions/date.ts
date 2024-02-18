018151f5d1e7e04d6e2b9f62442d9534e5970f82
Sito8943 <sito8943@gmail.com>
2024-02-18 16:10:25 -0500

    created function for countdown

--- src/utils/functions/date.ts
+++ src/utils/functions/date.ts
@@ -9,3 +9,30 @@ export const parseDate = (dateString: string) => {
     year: "numeric",
   });
 };
+
+type countdownType = {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+
+export const countdown = (endDate: number) => {
+  let timeLeft = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  if (endDate > 0) {
+    timeLeft = {
+      days: Math.floor(endDate / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((endDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((endDate % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((endDate % (1000 * 60)) / 1000),
+    };
+  }
+
+  return timeLeft;
+};
