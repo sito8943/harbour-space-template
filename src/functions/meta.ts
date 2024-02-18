a05d84412cfcbe00f89671261ae418a8ff9c6fad
Sito8943 <sito8943@gmail.com>
2024-02-18 08:33:31 -0500

    added id attribute for remove meta tags after cleanup

--- src/functions/meta.ts
+++ src/functions/meta.ts
@@ -1,6 +1,12 @@
 export const addMeta = (key: string, content: string) => {
   const metaElement = document.createElement("meta");
+  metaElement.setAttribute("id", key);
   metaElement.setAttribute("name", key);
   metaElement.setAttribute("content", content);
   document.head.appendChild(metaElement);
+};
+
+export const removeMeta = (key: string) => {
+  const metaElement = document.getElementById(key);
+  if (metaElement) document.head.removeChild(metaElement);
 };
