019d29470d165e855e719c318556c3d0bd149a11
Sito8943 <sito8943@gmail.com>
2024-02-18 12:15:38 -0500

    created index file for Providers with all exports

---
+++ src/providers/index.ts
@@ -0,0 +1,10 @@
+import {
+  ScholarshipProvider,
+  useScholarship,
+} from "./Scholarship/ScholarshipProvider";
+import {
+  HarbourSpaceApiClientProvider,
+  useHarbourSpaceApiClient,
+} from "./HarbourSpaceApiProvider";
+
+export { ScholarshipProvider };
