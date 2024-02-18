ce2a1211a94df37471ee6b889647d3eb7dd8bb9c
Sito8943 <sito8943@gmail.com>
2024-02-18 12:43:16 -0500

    created index file with api clients and react query keys

---
+++ src/utils/api/index.ts
@@ -0,0 +1,4 @@
+import { HarbourSpaceApiClient, ScholarshipApiClient } from "./clients";
+import { ReactQueryKeys } from "./queryKey";
+
+export { ReactQueryKeys, HarbourSpaceApiClient, ScholarshipApiClient };
