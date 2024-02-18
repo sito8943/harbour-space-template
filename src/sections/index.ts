c117ed974f4298a4cc1350f7e922a60a20d9ad56
Sito8943 <sito8943@gmail.com>
2024-02-18 15:02:34 -0500

    added NotFound to index of sections

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -2,5 +2,6 @@ import About from "./About/About";
 import Hero from "./Hero/Hero";
 import Testimonials from "./Testimonials/Testimonials";
 import Faqs from "./Faqs/Faqs";
+import NotFound from "./NotFound/NotFound";
 
-export { About, Hero, Testimonials, Faqs };
+export { About, Hero, Testimonials, Faqs, NotFound };
