5abc2bc5fa765e1673846d50e59c55ca1b6aab89
Sito8943 <sito8943@gmail.com>
2024-02-18 16:14:37 -0500

    exporting Countdown at index file for components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Column from "./Column/Column";
 import Navbar from "./Navbar/Navbar";
 import SplashScreen from "./SplashScreen/SplashScreen";
 import StickyBar from "./StickyBar/StickyBar";
+import Countdown from "./Countdown/Countdown";
 
-export { Card, Column, Navbar, SplashScreen, StickyBar };
+export { Card, Column, Navbar, SplashScreen, StickyBar, Countdown };
