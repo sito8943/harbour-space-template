4f9b7fa9760318d2dfb61a2fddd3e9db57d3f03b
Sito8943 <sito8943@gmail.com>
2024-02-18 12:44:23 -0500

    created HarbourSpaceApiProvider

---
+++ src/providers/HarbourSpaceApiProvider.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, createContext, useContext } from "react";
+import { HarbourSpaceApiClient } from "utils/api/clients/HarbourSpaceApiClient";
+
+/** KatanaApiClientContextProps */
+export interface HarbourSpaceApiClientContextProps {
+  client: HarbourSpaceApiClient;
+  children?: ReactNode;
+}
+
+/** HarbourSpaceApiClientProviderProps */
+export type HarbourSpaceApiClientProviderProps =
+  HarbourSpaceApiClientContextProps;
+
+const HarbourSpaceApiClientContext = createContext(
+  {} as HarbourSpaceApiClientContextProps
+);
+
+/**
+ * HarbourSpaceApiClientProvider
+ * @param props Component props
+ * @returns Component
+ */
+const HarbourSpaceApiClientProvider = (
+  props: HarbourSpaceApiClientProviderProps
+) => {
+  const lifeLineApiClient = new HarbourSpaceApiClient();
+
+  return (
+    <HarbourSpaceApiClientContext.Provider
+      value={{ client: lifeLineApiClient }}
+    >
+      {props.children}
+    </HarbourSpaceApiClientContext.Provider>
+  );
+};
+
+const useHarbourSpaceApiClient = (): HarbourSpaceApiClient => {
+  const context = useContext(HarbourSpaceApiClientContext);
+  if (!context)
+    throw new Error("scholarshipContext must be used within a Provider");
+  return context.client;
+};
+
+export { HarbourSpaceApiClientProvider, useHarbourSpaceApiClient };
