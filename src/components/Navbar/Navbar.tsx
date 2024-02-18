18f68573786e48b74cd0e59ad8d472742aff69ad
Sito8943 <sito8943@gmail.com>
2024-02-17 20:44:37 -0500

    added menu button and set to it in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 // images
+import menu from "../../assets/images/menu.png";
 import logo from "../../assets/images/logo.svg";
 
 // styles
@@ -7,13 +8,17 @@ import "./styles.css";
 function Navbar() {
   return (
     <header className="w-full h-[130px] relative">
-      <div className="bg-primary flex items-center justify-between h-[65px] px-10 text-white">
+      <div className="bg-primary flex items-center justify-between h-[65px] px-10 text-white ipad:px-5 mobile:px-3">
         <div className="flex items-start gap-2">
           <img src={logo} alt="Harbour.Space's logo" />
-          <span className="uppercase leading-[18px]">/Interaction Design</span>
+          <span className="uppercase leading-[18px] ipad:text-[11px] ipad:leading-[11px]">
+            /Interaction Design
+          </span>
         </div>
         <div>
-          <button name="menu-button"></button>
+          <button name="menu-button" aria-label="click to open navigation menu">
+            <img src={menu} alt="Icon of a hamburger menu button" />
+          </button>
         </div>
         <a
           href="#"
