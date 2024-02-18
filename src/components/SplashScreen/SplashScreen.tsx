30267baa98a127e322119761b2f7b4c903eebd7e
Sito8943 <sito8943@gmail.com>
2024-02-18 15:03:37 -0500

    added animation to splash screen

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -1,10 +1,30 @@
+import { css } from "@emotion/css";
+
+// images
+import logo from "assets/images/logo-primary.svg";
+
+// styles
+import "./styles.css";
+
 type SplashScreenPropType = {
   isLoading: boolean;
   error: Error | null;
 };
 
 function SplashScreen(props: SplashScreenPropType) {
-  return <div></div>;
+  const { isLoading, error } = props;
+
+  return (
+    <div
+      className={`w-full h-screen flex items-center justify-center bg-white transition ${!isLoading ? "opacity-0 pointer-events-none" : "opacity-100"}`}
+    >
+      <img
+        className={`w-80 h-80 blink`}
+        src={logo}
+        alt="Harbour.Space's logo"
+      />
+    </div>
+  );
 }
 
 export default SplashScreen;
