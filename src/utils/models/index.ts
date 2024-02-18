3fff5f8b4016a7842cb8a6d06237714d7e42bfef
Sito8943 <sito8943@gmail.com>
2024-02-18 10:14:09 -0500

    created index with common exports for dto and models

---
+++ src/utils/models/index.ts
@@ -0,0 +1,33 @@
+import { HtmlItem, HtmlList, HtmlParagraph } from "./HtmlItem";
+import { HtmlMetaTag, OGMetaTag } from "./MetaTags";
+import {
+  ApplicationProcess,
+  Company,
+  Country,
+  Director,
+  Faq,
+  Location,
+  PhotoFile,
+  Program,
+  Scholarship,
+  Testimonial,
+} from "./dto";
+export type {
+  // dto
+  ApplicationProcess,
+  Company,
+  Country,
+  Director,
+  Faq,
+  Location,
+  PhotoFile,
+  Program,
+  Scholarship,
+  Testimonial,
+  // ui
+  HtmlItem,
+  HtmlList,
+  HtmlParagraph,
+  HtmlMetaTag,
+  OGMetaTag,
+};
