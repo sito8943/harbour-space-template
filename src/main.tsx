b944ede98b3b08d87ef94e0c4010f67f3b729100
Sito8943 <sito8943@gmail.com>
2024-02-18 12:52:22 -0500

    added HarbourSpaceApiClientProvider to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import ReactDOM from "react-dom/client";
+
+// utils
+import { HarbourSpaceApiClient } from "utils";
+
+// providers
+import { HarbourSpaceApiClientProvider } from "providers";
+
+// views
 import ScholarshipView from "./ScholarshipView";
+
+// styles
 import "./index.css";
 
 const queryClient = new QueryClient();
 
+const harbourSpaceClient = new HarbourSpaceApiClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <ScholarshipView />
+    <HarbourSpaceApiClientProvider client={harbourSpaceClient}>
+      <ScholarshipView />
+    </HarbourSpaceApiClientProvider>
   </QueryClientProvider>
 );
