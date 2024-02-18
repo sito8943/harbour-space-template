c9489d4bb3483986a28d7f73e624366d6096a058
Sito8943 <sito8943@gmail.com>
2024-02-18 15:52:51 -0500

    using api data in hero grid card

--- src/sections/Hero/components/GridCard.tsx
+++ src/sections/Hero/components/GridCard.tsx
@@ -1,27 +1,39 @@
 // components
 import { Card } from "components";
 
+// providers
+import { useScholarship } from "providers";
+
+// utils
+import { parseDate } from "utils";
+
 function GridCard() {
+  const { scholarship } = useScholarship();
+
   return (
     <Card className="grid grid-cols-2 w-full gap-2 bg-white z-10">
       <div>
         <h3>Location</h3>
-        <p className="card-content">Bangkok</p>
+        <p className="card-content">{scholarship.location.name}</p>
       </div>
       <div>
         <h3>Duration</h3>
         <p className="card-content">
-          1 Year <br />
+          {scholarship.duration} Year <br />
           Full-Time
         </p>
       </div>
       <div>
         <h3>Start date</h3>
-        <p className="card-content">30 June 2020</p>
+        <p className="card-content">
+          {parseDate(scholarship.scholarship_start_date)}
+        </p>
       </div>
       <div>
         <h3>End date</h3>
-        <p className="card-content">3 Aug 2020</p>
+        <p className="card-content">
+          {parseDate(scholarship.application_end_date)}
+        </p>
       </div>
     </Card>
   );
