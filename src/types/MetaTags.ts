09edcc604db9ceb068aecb942c6b8200814ab1d8
Sito8943 <sito8943@gmail.com>
2024-02-18 08:21:19 -0500

    created MetaTags types and Testimonial type

---
+++ src/types/MetaTags.ts
@@ -0,0 +1,17 @@
+export type HtmlMetaTag = {
+  id: number;
+  scope: string;
+  title: string;
+  description: string;
+  abstract: string;
+  keywords: string;
+};
+
+export type OGMetaTag = {
+  id: number;
+  scope: string;
+  page_url: string;
+  title: string;
+  desc: string;
+  image_url: string;
+};
