1cf688e98d8d9899ed844a6cfbfcbbb33bdc46ed
Sito8943 <sito8943@gmail.com>
2024-02-19 10:34:40 -0500

    making slider parent

--- src/sections/Testimonials/Testimonials.tsx
+++ src/sections/Testimonials/Testimonials.tsx
@@ -1,5 +1,29 @@
+// images
+import rectangleVector from "assets/images/rectangle-vector.svg";
+
+// components
+import { SliderItem } from "./components";
+
 function Testimonials() {
-  return <div></div>;
+  return (
+    <section
+      id="testimonials"
+      className="section w-full flex mb-20 relative !px-0"
+    >
+      <div className="absolute top-0 left-[50%] -translate-x-[50%] w-full flex justify-center ipadPro:bg-primary">
+        <img
+          src={rectangleVector}
+          className="image-borders w-[80%] h-[395px] object-cover"
+          alt="A set of vectors arranged in a rectangle pattern."
+        />
+      </div>
+      <div className="z-10 flex items-center justify-center w-[100vw] overflow-hidden gap-10 ipadPro:gap-9 ipad:gap-6 mobile:gap-5">
+        <SliderItem />
+        <SliderItem />
+        <SliderItem />
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials;
---
+++ src/sections/Testimonials/components/Slider.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Slider() {
+  return (
+    <div className="z-10 flex items-center justify-center w-[100vw] overflow-hidden gap-10 ipadPro:gap-9 ipad:gap-6 mobile:gap-5"></div>
+  );
+}
+
+export default Slider;
