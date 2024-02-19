d4e3fff17604299941d5d627ef897139a81a3927
Sito8943 <sito8943@gmail.com>
2024-02-18 20:55:48 -0500

    using Menu component in Faq

--- src/sections/Faqs/Faqs.tsx
+++ src/sections/Faqs/Faqs.tsx
@@ -1,10 +1,7 @@
 import { useMemo, useState } from "react";
 
-// images
-import chevronDown from "assets/images/chevron-down.svg";
-
 // components
-import { Accordion } from "./components";
+import { Accordion, Menu } from "./components";
 
 // providers
 import { useScholarship } from "providers";
@@ -13,24 +10,6 @@ function Faqs() {
   const { faqs, faqTypes } = useScholarship();
 
   const [filterBy, setFilterBy] = useState<number>(0);
-
-  const renderFaqTypes = useMemo(
-    () =>
-      faqTypes
-        .filter((_, i) => i !== filterBy)
-        .map((filter, i) => (
-          <li key={filter}>
-            <button
-              onClick={() => setFilterBy(i)}
-              name={`${filter.toLowerCase()}`}
-              aria-label={`filter by ${filter.toLowerCase()}`}
-            >
-              {filter}
-            </button>
-          </li>
-        )),
-    [filterBy]
-  );
 
   const filteredFaqs = useMemo(
     () => faqs.filter((faq) => faq.type === faqTypes[filterBy]),
@@ -48,25 +27,11 @@ function Faqs() {
         </h2>
         <div className="flex items-center gap-10 ipadPro:w-full ipad:flex-col ipad:items-start mobile:gap-2 ">
           <p className="w-[100px]">Filter by:</p>
-          <div className="ipadPro:w-full">
-            <button
-              name="filter-menu"
-              aria-label="click to open filter menu"
-              className="flex items-center justify-between gap-2 border rounded-[100px] py-3 px-5 ipadPro:w-full"
-            >
-              <p className="text-primary font-semibold ipadPro:text-lg">
-                {faqTypes[filterBy]}
-              </p>
-              <div className="w-[30px] h-[30px] flex items-center justify-center">
-                <img
-                  className="w-4 h-4 ipadPro:w-3 ipadPro:h-3"
-                  src={chevronDown}
-                  alt="Downward-pointing chevron icon"
-                />
-              </div>
-            </button>
-            <div className="hidden">{renderFaqTypes}</div>
-          </div>
+          <Menu
+            selected={filterBy}
+            onChange={(active: number) => setFilterBy(active)}
+            items={faqTypes}
+          />
         </div>
       </div>
       <Accordion items={filteredFaqs} />
