d31ce10677f7159b8f053c7be651bd0f0f1554f8
Sito8943 <sito8943@gmail.com>
2024-02-18 21:29:05 -0500

    creating logic of Faq Categories Menu

--- src/sections/Faqs/components/Menu/Menu.tsx
+++ src/sections/Faqs/components/Menu/Menu.tsx
@@ -1,7 +1,11 @@
 import { useMemo, useState } from "react";
+import { css } from "@emotion/css";
 
 // images
 import chevronDown from "assets/images/chevron-down.svg";
+
+// styles
+import "./styles.css";
 
 type MenuPropType = {
   items: string[];
@@ -19,10 +23,17 @@ function Menu(props: MenuPropType) {
       items
         .filter((_, i) => i !== selected)
         .map((filter, i) => (
-          <li key={filter}>
+          <li
+            key={filter}
+            className="flex items-center justify-between gap-2 py-3 "
+          >
             <button
-              onClick={() => onChange(i)}
+              onClick={() => {
+                setOpened(false);
+                onChange(i);
+              }}
               name={`${filter.toLowerCase()}`}
+              className={`text-gray font-semibold text-lg ${i === 0 ? "mt-2" : ""}`}
               aria-label={`filter by ${filter.toLowerCase()}`}
             >
               {filter}
@@ -32,17 +43,22 @@ function Menu(props: MenuPropType) {
     [selected, items]
   );
 
+  const gridTemplateRowsCSS = useMemo(
+    () => css({ gridTemplateRows: `35px ${opened ? "1fr" : "0fr"}` }),
+    [opened]
+  );
+
   return (
-    <div className="ipadPro:w-full">
+    <div
+      className={`menu ipadPro:w-full grid relative ${gridTemplateRowsCSS} border ${opened ? "rounded-[40px]" : "rounded-[100px]"} py-3 px-5`}
+    >
       <button
         name="filter-menu"
         onClick={() => setOpened((opened) => !opened)}
         aria-label="click to open filter menu"
-        className="flex items-center justify-between gap-2 border rounded-[100px] py-3 px-5 ipadPro:w-full"
+        className="flex items-center justify-between gap-2 ipadPro:w-full"
       >
-        <p className="text-primary font-semibold ipadPro:text-lg">
-          {items[selected]}
-        </p>
+        <p className="text-primary font-semibold text-lg">{items[selected]}</p>
         <div className="w-[30px] h-[30px] flex items-center justify-center">
           <img
             className={`w-4 h-4 ipadPro:w-3 ipadPro:h-3 ${opened ? "rotate-180" : "rotate-0"}`}
@@ -51,7 +67,7 @@ function Menu(props: MenuPropType) {
           />
         </div>
       </button>
-      <div className="hidden">{renderItems}</div>
+      <div className="overflow-hidden">{renderItems}</div>
     </div>
   );
 }
