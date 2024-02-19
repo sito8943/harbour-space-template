34be0d1982427681cb84a9c54d05a3b23509e105
Sito8943 <sito8943@gmail.com>
2024-02-18 21:58:43 -0500

    create views folder with Scholarship and Home

---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/data-science-apprenticeship-zeptolab">Load</Link>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/views/index.ts
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import ScholarshipView from "./ScholarshipView";
+
+export { Home, ScholarshipView };
