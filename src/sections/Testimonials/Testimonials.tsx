4e0e77362db5d9599086927b0e97bec59c29f7ea
Sito8943 <sito8943@gmail.com>
2024-02-19 18:59:00 -0500

    added rectangle vector in primary background

--- src/sections/Testimonials/Testimonials.tsx
+++ src/sections/Testimonials/Testimonials.tsx
@@ -1,5 +1,8 @@
+import { css } from "@emotion/css";
+
 // images
 import rectangleVector from "assets/images/rectangle-vector.svg";
+import primaryRectangleVector from "assets/images/main-rectangle.png";
 
 // components
 import { SliderItem } from "./components";
@@ -11,10 +14,13 @@ function Testimonials() {
       className="section w-full flex mb-20 relative !px-0"
     >
       <div className="absolute top-0 left-[50%] -translate-x-[50%] w-full flex justify-center ipadPro:bg-primary">
-        <img
-          src={rectangleVector}
-          className="image-borders w-[80%] h-[395px] object-cover"
-          alt="A set of vectors arranged in a rectangle pattern."
+        <div
+          className={`image-borders w-[80%] h-[395px] ipadPro:w-full ipadPro:border-none ${css({
+            backgroundImage: `url(${rectangleVector})`,
+            "@media (max-width: 834px)": {
+              backgroundImage: `url(${primaryRectangleVector})`,
+            },
+          })}`}
         />
       </div>
       <div className="z-10 flex items-center justify-center w-[100vw] overflow-hidden gap-10 ipadPro:gap-9 ipad:gap-6 mobile:gap-5">
