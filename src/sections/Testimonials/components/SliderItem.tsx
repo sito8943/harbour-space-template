65b400ffb1701b8b27242025a5f4b747e8cca929
Sito8943 <sito8943@gmail.com>
2024-02-19 08:40:01 -0500

    create Slider Item component with its styles

---
+++ src/sections/Testimonials/components/SliderItem.tsx
@@ -0,0 +1,45 @@
+// images
+import irene from "assets/images/irene.png";
+import linkedin from "assets/images/linkedin.png";
+
+// styles
+import "./styles.css";
+
+function SliderItem() {
+  return (
+    <div className="slider-item">
+      <div className="top card-borders">
+        <div className="person">
+          <img
+            src={irene}
+            className="w-20 h-20"
+            alt="A picture of an Asian girl gracefully holding her long black hair."
+          />
+          <div>
+            <h4>Irene Pereyra</h4>
+            <p>Interaction Design Fellow '19</p>
+          </div>
+        </div>
+        <div>
+          <a href="www.linkedin.com" target="_blank" rel="noreferrer">
+            <img src={linkedin} alt="linkedin's logo" />
+          </a>
+        </div>
+      </div>
+      <div className="bottom card-borders">
+        <p className="bottom-content">
+          This Fellowship was a turning point in my career. I wouldn't be where
+          I am today without the financial support and experienced offered
+          through the program.
+        </p>
+        <div className="bottom-extra">
+          <p>Education</p>
+          <p>·</p>
+          <p>B.A. Visual Design</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SliderItem;
