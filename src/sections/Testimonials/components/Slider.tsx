a000d357fd12a032daf9f0220ba46cba9b602c18
Sito8943 <sito8943@gmail.com>
2024-02-20 08:15:22 -0500

    added move (forward and backward) of slider and responsive displacement

--- src/sections/Testimonials/components/Slider.tsx
+++ src/sections/Testimonials/components/Slider.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { css } from "@emotion/css";
 
 // images
 import chevron from "assets/images/chevron-down.svg";
@@ -16,23 +17,69 @@ type SliderProp = {
 function Slider(props: SliderProp) {
   const { items } = props;
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const onWindowResize = useCallback(
+    () => setWindowWidth(window.innerWidth),
+    [setWindowWidth]
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", onWindowResize);
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+    };
+  }, [onWindowResize]);
+
   const renderItems = useMemo(
     () => items.map((item) => <SliderItem key={item.id} {...item} />),
     [items]
   );
 
+  const [displacement, setDisplacement] = useState(0);
+
+  const currentSizeDisplacementOffset = useMemo(() => {
+    switch (true) {
+      case windowWidth <= 375:
+        return 350;
+      case windowWidth <= 768:
+        return 400;
+      case windowWidth <= 834:
+        return 735;
+      default:
+        return 840;
+    }
+  }, [windowWidth]);
+
+  const transformMovement = useMemo(
+    () =>
+      css({
+        transform: `translateX(${currentSizeDisplacementOffset * displacement}px)`,
+      }),
+    [currentSizeDisplacementOffset, displacement]
+  );
+
+  const moveForward = () => setDisplacement(displacement - 1);
+
+  const moveBackward = () => setDisplacement(displacement + 1);
+
   return (
     <div className="slider">
-      <div className="slider-content">{renderItems}</div>
+      <div className={`slider-content ${transformMovement}`}>{renderItems}</div>
       <div className="slider-navigation">
-        <button className="slider-nav-button image-borders">
+        <button
+          onClick={moveBackward}
+          className="slider-nav-button image-borders"
+        >
           <img
             className="w-4 h-4 rotate-90"
             src={chevron}
             alt="leftward pointing chevron icon"
           />
         </button>
-        <button className="slider-nav-button image-borders">
+        <button
+          onClick={moveForward}
+          className="slider-nav-button image-borders"
+        >
           <img
             className="w-4 h-4 rotate-[270deg]"
             src={chevron}
