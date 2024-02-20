c088a7a76da6775bfb7e7d77ac0003beacf68535
Sito8943 <sito8943@gmail.com>
2024-02-20 08:15:34 -0500

    added the fifth card to template slider

--- src/sections/Testimonials/Testimonials.tsx
+++ src/sections/Testimonials/Testimonials.tsx
@@ -37,7 +37,7 @@ function Testimonials() {
         items={
           testimonials.length
             ? testimonials
-            : [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+            : [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
         }
       />
     </section>
