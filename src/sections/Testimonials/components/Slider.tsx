c9e2c13c5497efc2545a356bc6cc0b7ae418e818
Sito8943 <sito8943@gmail.com>
2024-02-19 20:50:45 -0500

    using SliderItems and renderItems in Slider

--- src/sections/Testimonials/components/Slider.tsx
+++ src/sections/Testimonials/components/Slider.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import { useMemo } from "react";
 
-function Slider() {
+// images
+import chevronDown from "assets/images/chevron-down.svg";
+
+// utils
+import { Testimonial } from "utils";
+
+// components
+import SliderItem from "./SliderItem";
+
+type SliderProp = {
+  items: Testimonial[];
+};
+
+function Slider(props: SliderProp) {
+  const { items } = props;
+
+  const renderItems = useMemo(
+    () => items.map((item) => <SliderItem key={item.id} {...item} />),
+    [items]
+  );
+
   return (
-    <div className="z-10 flex items-center justify-center w-[100vw] overflow-hidden gap-10 ipadPro:gap-9 ipad:gap-6 mobile:gap-5"></div>
+    <div className="z-10 flex items-center justify-center w-[100vw] overflow-hidden gap-10 ipadPro:gap-9 ipad:gap-6 mobile:gap-5">
+      {renderItems}
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
   );
 }
 
